6888612e16cbad923d354fe49693534992b3a867
Daniele Nocito <danielito@Danieles-MacBook-Pro.local>
2019-06-16 17:51:18 +0100

    created game logic second

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,117 @@
 //console.log("Up and running!");
-//console.log("User flipped " + cardOne);
+
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0]
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+var cardOne = cards[0];
 var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay[0] === cardsInPlay[2]) {
-alert("You found a match!");
+
+console.log("User flipped " + cardsInPlay);
+
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("You did not find any match!(as expected)");
 }
-else {
-alert("Sorry, try again.");
+
+
+/*
+document.addEventListener('DOMContentLoaded', fn, false);
+
+function fn() {
+
+	console.log("Ready")
+
+	var main = document.getElementById("game-board").appendChild("<p>Hello</p>")
+
+	console.log(main)
+
+	// main.appendChild("<p>Hello</p>")
 }
+
+
+
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
+
+
+
+
+var cardsInPlay = [];
+
+
+function createBoard (){
+	for (var i = 0; i < cards.length; i++) {
+	 var cardElement = ""
+	
+	}
+
+
+	document.addEventListener("click", function(){
+
+	});
+};
+
+
+
+function checkForMatch (){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+
+function flipCard (){
+    // Method
+    this.getAttribute()
+        console.log("inside get getAttribute");
+}
+
+// var cardElement = document.createElement('img');
+// cardElement.setAttribute('attributeName', 'attributeValue');
+// setAttribute('data-id');
+
+
+// //invoke createBoard function
+// cards.createBoard();
+
+
+// console.log("User flipped " + cards[cardId].rank);
+// cardsInPlay.push(cards[cardId].rank);
+
+// console.log(cardImage)
+// console.log(suit)
+
+// createBoard ();
+
+// }
+*/
+
+
+
+
